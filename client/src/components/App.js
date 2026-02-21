072ba63fecdb668d8acaa633d82aba1a09201f40
njoroofficial <mosesn579@gmail.com>
2026-02-21 10:00:57 +0300

    Update API endpoint URLs and implement message CRUD operations

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,7 +12,7 @@ function App() {
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetch("http://127.0.0.1:4000/messages")
+    fetch("http://127.0.0.1:5555/messages")
       .then((r) => r.json())
       .then((messages) => setMessages(messages));
   }, []);
@@ -38,7 +38,7 @@ function App() {
   }
 
   const displayedMessages = messages.filter((message) =>
-    message.body.toLowerCase().includes(search.toLowerCase())
+    (message.body || "").toLowerCase().includes(search.toLowerCase()),
   );
 
   return (
--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response
 from flask_cors import CORS
 from flask_migrate import Migrate
-
 from models import db, Message
 
 app = Flask(__name__)
@@ -14,13 +13,95 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+# get all messages
+@app.route('/messages', methods=['GET'])
 def messages():
-    return ''
+    messages = []
+    for message in Message.query.all():
+        messages.append(message.to_dict())
+    
+    response =  make_response(
+        messages,
+        200
+    )
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+    return response
+
+# get message by id
+@app.route('/messages/<int:message_id>', methods=['GET'])
+def messages_by_id(message_id):
+    message = Message.query.filter(Message.id == message_id).first()
+
+    if message:
+        body = message.to_dict()
+        status = 200
+    else:
+        body = {'message':f'Message {message_id} not found'}
+        status = 404
+
+    return make_response(body, status)
+
+# Create a new message
+@app.route("/messages", methods=['POST'])
+def create_message():
+    new_message = Message(
+        body = request.json.get("body"),
+        username = request.json.get("username")
+        )
+    
+    db.session.add(new_message)
+    db.session.commit()
+
+    return make_response(new_message.to_dict(), 201)
+
+# update a message
+@app.route("/messages/<int:message_id>", methods=['PATCH'])
+def update_message(message_id):
+    message = Message.query.filter(Message.id == message_id).first()
+
+    if message:
+        for attr in request.json:
+            setattr(message, attr, request.json.get(attr))
+
+        db.session.add(message)
+        db.session.commit()
+
+        response = make_response(
+            message.to_dict(),
+            200
+        )
+    else:
+        response = make_response(
+            {"message":"Message not found"},
+            404
+        )
+
+    return response
+
+# Delete a message
+@app.route("/messages/<int:message_id>", methods=['DELETE'])
+def delete_message(message_id):
+    message = Message.query.filter(Message.id == message_id).first()
+
+    if message:
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+                "delete_successful": True,
+                "message": "Message deleted."
+            }
+        response = make_response(
+                response_body,
+                200
+            )
+    else:
+        response = make_response (
+            {"Message": "Error deleting the message"},
+            404
+        )
+    
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
